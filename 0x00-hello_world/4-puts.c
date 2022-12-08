4bd11af1665da6ee4743491ac7f00fb5820a9327
Pascal <persiewailer697@gmail.com>
2022-12-08 11:34:45 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
+ *  * main - print to screen
  *
- *  * main - Entry point
+ *  *
  *
- *   *
- *
- *    * Return: Always 0 (Success)
+ *  * Return: zero
  *
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
 	return (0);
 }
